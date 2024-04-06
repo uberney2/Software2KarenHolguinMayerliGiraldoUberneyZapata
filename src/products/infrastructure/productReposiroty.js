38a39d58cc5e01df61f8c2eb183eb63ada1a473e
Uberney Zapata <uzapatz@contratista.epm.co>
2024-04-06 17:08:16 -0500

    CHG add validation for serching existing product

--- src/products/infrastructure/productReposiroty.js
+++ src/products/infrastructure/productReposiroty.js
@@ -1,24 +1,30 @@
-const { productModel } = require('./product');
+const { productModel } = require("./product");
 
-async function createProductRepository(productRequest){
-    try {
-        console.log('product')
-        console.log(productRequest)
-        const product = new productModel({
-            name: productRequest.name,
-            description: productRequest.description,
-            url: productRequest.url,
-            tags: productRequest.tags,
-            createdAt: productRequest.createdAt,
-            updatedAt: productRequest.updatedAt
-          });
-    
-        const save = await product.save();
-        return save
-    } catch (error) {
-        return error;
-    }
-    
+async function createProductRepository(productRequest) {
+  try {
+    const product = new productModel({
+      name: productRequest.name,
+      description: productRequest.description,
+      url: productRequest.url,
+      tags: productRequest.tags,
+      createdAt: productRequest.createdAt,
+      updatedAt: productRequest.updatedAt,
+    });
+
+    const save = await product.save();
+    return save;
+  } catch (error) {
+    return error;
+  }
 }
 
-module.exports = {createProductRepository}
+async function getProductRepositoryByName(name){
+  try {
+    const product = await productModel.findOne({ name: name });
+    return product;
+  } catch (error) {
+    return error;
+  }
+}
+
+module.exports = { createProductRepository, getProductRepositoryByName };
