73f7eab2d57304464c01c30f24e03fe51c545980
karenhol7 <karenhol137@gmail.com>
2024-04-11 08:32:37 -0500

    CHG modify exception to reflect that a product has no comments

--- src/comments/exceptions/commentNotFound.js
+++
@@ -1,9 +0,0 @@
-class ExcepcioncommentNotFound extends Error{
-
-    constructor(id){
-        
-        super(`comment with id: ${id} not found`)
-    }
-}
-
-module.exports = {ExcepcioncommentNotFound};
