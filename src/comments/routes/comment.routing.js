244c800bd4d2d9a4c2570dd383ce92948ed3e72e
Uberney Zapata <uzapatz@contratista.epm.co>
2024-04-12 16:12:18 -0500

    ADD middleware for portect routes

--- src/comments/routes/comment.routing.js
+++ src/comments/routes/comment.routing.js
@@ -1,7 +1,8 @@
 commentRouter = require('express').Router();
 const { createComment, getAvgRating } = require('../api/comment.controller');
+const verifyToken = require('../../middleware/authMiddleware');
 
-commentRouter.post('/createComment', createComment);
-commentRouter.get('/average-raiting/:productId', getAvgRating);
+commentRouter.post('/createComment', verifyToken, createComment);
+commentRouter.get('/average-raiting/:productId', verifyToken, getAvgRating);
 
 module.exports = commentRouter;
