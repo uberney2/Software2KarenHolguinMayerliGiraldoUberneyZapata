75650ec9b7634c1eb2f304433120a8df3d11c476
Uberney Zapata <uzapatz@contratista.epm.co>
2024-04-12 19:15:19 -0500

    ADD menssage error

--- src/middleware/createProductValidationMeddleware.js
+++ src/middleware/createProductValidationMeddleware.js
@@ -2,7 +2,7 @@ const validateProductFields = (req, res, next) => {
     const { name, description, rate, category, url, tags } = req.body;
     
     if (!name || !description || !rate || !category || !url || !tags) {
-      return res.status(400).json({ message: "Todos los campos del producto son obligatorios." });
+      return res.status(400).json({ message: "All fields should be mandatory." });
     }
   
     next();
