3a9868cb56b73e2b4d4e1abda7618fd6c93917dc
karenhol7 <karenhol137@gmail.com>
2024-04-12 21:54:13 -0500

    ADD exception handling to product details fetch

--- src/products/application/detail-product.js
+++ src/products/application/detail-product.js
@@ -1,8 +1,15 @@
 const { getProductDetailRepository } = require('../infrastructure/productReposiroty');
+const { ProductNotFound } = require('../exceptions/productNotFound');
 
 async function getProductDetaislUseCase(productId) {
 
     const productDetails = await getProductDetailRepository(productId);
+
+    if (!productDetails.detailProduct) {
+
+        throw new ProductNotFound(productId);
+    }
+
     return productDetails;
 
 }
