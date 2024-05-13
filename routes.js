53297ca2154e0a4027f660e4adb485c5d7ff2896
Uberney Zapata <uzapatz@contratista.epm.co>
2024-05-13 18:26:34 -0500

    policy cors

--- routes.js
+++ routes.js
@@ -1,11 +1,27 @@
-const taskRouter = require('./src/products/routes/product.routing')
-const userRouter = require('./src/user/routes/user.routing')
-const commentRouter= require('./src/comments/routes/comment.routing')
+const taskRouter = require('./src/products/routes/product.routing');
+const userRouter = require('./src/user/routes/user.routing');
+const commentRouter = require('./src/comments/routes/comment.routing');
 
 function setRoutes(app) {
-    app.use('/api/product', taskRouter)
-    app.use('/api/user', userRouter)
-    app.use('/api/comment', commentRouter)
+    app.use((req, res, next) => {
+        console.log(`request Method: ${req.method}`);
+        console.log(`request URL: ${req.originalUrl}`);
+        res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Permitir solicitudes desde el origen del frontend
+        res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+        res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE, PATCH');
+        
+        // Manejar solicitudes OPTIONS de preflight
+        if (req.method === 'OPTIONS') {
+            res.sendStatus(200);
+        } else {
+            next();
+        }
+    });
+    
+    app.use('/api/product', taskRouter);
+    app.use('/api/user', userRouter);
+    app.use('/api/comment', commentRouter);
 }
 
 module.exports = setRoutes;
