5b644eee72052554ec2630b5b1e2f1922c409c16
Uberney Zapata <uzapatz@contratista.epm.co>
2024-05-17 15:58:41 -0500

    erased exception

--- src/products/application/search-product.js
+++ src/products/application/search-product.js
@@ -4,11 +4,6 @@ const { ProductNotFound } = require('../exceptions/productNotFound');
 async function searchProductByCriteriaUseCase(criteria) {
     try {
         const products = await getProductRepositoryByCriteria(criteria);
-        
-        if (!products || products.length === 0) {
-            throw new ProductNotFound(criteria.id); 
-        }
-        
         return products;
     } catch (error) {
         throw error;
